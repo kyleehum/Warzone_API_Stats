efb9894080a3db9d132c3b242dc40f63edfb6182
Kyle Hum <74889633+kyleehum@users.noreply.github.com>
2021-04-06 12:35:57 -0700

    Add files via upload

--- WarzoneStats.py
+++ WarzoneStats.py
@@ -1,15 +1,24 @@
 import requests
 import pandas as pd
+import re
+import io
 
-#Kyle's Warzone Stats
+
+#Kyle's Overall Stats
 url = "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/kai_uhl/psn"
 headers = {
     'x-rapidapi-key': "dc34a85caemshe2f48db1fbb3572p15fd8bjsn3a20a35d4f3c",
     'x-rapidapi-host': "call-of-duty-modern-warfare.p.rapidapi.com"
     }
 results = requests.request("GET", url, headers=headers)
+cleanresults = results.text.replace('"', "")
+cleanedresults = cleanresults.replace(',', " ")
 
-print(results.text)
+print(cleanedresults)
+
+data = io.StringIO(cleanedresults)
+df = pd.read_csv(data, sep=" ")
+
 
 #Kyle's Weekly Stats
 url = "https://call-of-duty-modern-warfare.p.rapidapi.com/weekly-stats/kai_uhl/psn"
@@ -18,9 +27,17 @@ headers = {
     'x-rapidapi-key': "dc34a85caemshe2f48db1fbb3572p15fd8bjsn3a20a35d4f3c",
     'x-rapidapi-host': "call-of-duty-modern-warfare.p.rapidapi.com"
     }
-response = requests.request("GET", url, headers=headers)
+results = requests.request("GET", url, headers=headers)
+cleanresults = results.text.replace('"', "")
+cleanedresults = cleanresults.replace(',', " ")
 
-print(response.text)
+print(cleanedresults)
+
+#converts string to dataframe
+data = io.StringIO(cleanedresults)
+df = pd.read_csv(data, header = None, sep=" ")
+df.to_csv('kyleweeklystats224.csv')
+
 
 #Kevin's Weekly Stats
 url = "https://call-of-duty-modern-warfare.p.rapidapi.com/weekly-stats/kevynk/psn"
@@ -29,17 +46,61 @@ headers = {
     'x-rapidapi-key': "dc34a85caemshe2f48db1fbb3572p15fd8bjsn3a20a35d4f3c",
     'x-rapidapi-host': "call-of-duty-modern-warfare.p.rapidapi.com"
     }
-response = requests.request("GET", url, headers=headers)
+results = requests.request("GET", url, headers=headers)
+cleanresults = results.text.replace('"', "")
+cleanedresults = cleanresults.replace(',', " ")
 
-print(response.text)
+print(cleanedresults)
 
-#Kevin's Warzone Stats
+#converts string to dataframe
+data = io.StringIO(cleanedresults)
+df1 = pd.read_csv(data, header = None, sep=" ")
+df1.to_csv('kevinweeklystats224.csv')
+
+
+#Kevin's Overall Stats
 url = "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/kevynk/psn"
 headers = {
     'x-rapidapi-key': "dc34a85caemshe2f48db1fbb3572p15fd8bjsn3a20a35d4f3c",
     'x-rapidapi-host': "call-of-duty-modern-warfare.p.rapidapi.com"
     }
 results = requests.request("GET", url, headers=headers)
-
 print(results.text)
 
+#Dande's Warzone Stats
+url = "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/TomatoCultivator%234902131/acti"
+headers = {
+    'x-rapidapi-key': "dc34a85caemshe2f48db1fbb3572p15fd8bjsn3a20a35d4f3c",
+    'x-rapidapi-host': "call-of-duty-modern-warfare.p.rapidapi.com"
+    }
+results = requests.request("GET", url, headers=headers)
+print(results.text)
+
+#Dande's Warzone Stats
+url = "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/heal%1694/battle"
+headers = {
+    'x-rapidapi-key': "dc34a85caemshe2f48db1fbb3572p15fd8bjsn3a20a35d4f3c",
+    'x-rapidapi-host': "call-of-duty-modern-warfare.p.rapidapi.com"
+    }
+results = requests.request("GET", url, headers=headers)
+print(results.text)
+
+#Johnson's Overall Stats
+url = "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/appledoglol/psn"
+headers = {
+    'x-rapidapi-key': "dc34a85caemshe2f48db1fbb3572p15fd8bjsn3a20a35d4f3c",
+    'x-rapidapi-host': "call-of-duty-modern-warfare.p.rapidapi.com"
+    }
+results = requests.request("GET", url, headers=headers)
+print(results.text)
+
+
+#Johnson's Weekly Stats
+url = "https://call-of-duty-modern-warfare.p.rapidapi.com/weekly-stats/appledoglol/psn"
+
+headers = {
+    'x-rapidapi-key': "dc34a85caemshe2f48db1fbb3572p15fd8bjsn3a20a35d4f3c",
+    'x-rapidapi-host': "call-of-duty-modern-warfare.p.rapidapi.com"
+    }
+response = requests.request("GET", url, headers=headers)
+print(response.text)
